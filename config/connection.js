491ded06542d0cb77e3d5304776428d658643d77
Pablo Fernandez <pfdzm@users.noreply.github.com>
2019-12-21 12:09:32 +0100

    fix npm linting scripts

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+const mysql = require("mysql");
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -12,10 +12,10 @@ var connection = mysql.createConnection({
 // Make connection.
 connection.connect(function(err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error(`error connecting: ${err.stack}`);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log(`connected as id ${connection.threadId}`);
 });
 
 // Export connection for our ORM to use.
--- models/supermarket.js
+++ models/supermarket.js
@@ -1,24 +1,24 @@
 // Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
+const orm = require("../config/orm.js");
 
-var cat = {
-  all: function(cb) {
+const cat = {
+  all(cb) {
     orm.all("cats", function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
+  create(cols, vals, cb) {
     orm.create("cats", cols, vals, function(res) {
       cb(res);
     });
   },
-  update: function(objColVals, condition, cb) {
+  update(objColVals, condition, cb) {
     orm.update("cats", objColVals, condition, function(res) {
       cb(res);
     });
   },
-  delete: function(condition, cb) {
+  delete(condition, cb) {
     orm.delete("cats", condition, function(res) {
       cb(res);
     });
