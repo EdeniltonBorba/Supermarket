8e7e1b1823eb0b5487a005e4a079050309a3c15c
Edenilton <edeniltonborba.adm@gmail.com>
2020-01-10 11:06:26 +0100

    add connections.js

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: process.env.DB_PASS,
-  database: "cat_db"
+  database: "market_db"
 });
 
 // Make connection.
