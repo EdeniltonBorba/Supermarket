0dc91d9b491d75491ca9dbef291d63062ad1a39f
Edenilton <edeniltonborba.adm@gmail.com>
2020-01-10 16:44:48 +0100

    add handlebars

--- models/supermarket.js
+++ models/supermarket.js
@@ -1,25 +1,25 @@
 // Import the ORM to create functions that will interact with the database.
 const orm = require("../config/orm.js");
 
-const cat = {
+const products = {
   all(cb) {
-    orm.all("cats", function(res) {
+    orm.all("products", function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
   create(cols, vals, cb) {
-    orm.create("cats", cols, vals, function(res) {
+    orm.create("products", cols, vals, function(res) {
       cb(res);
     });
   },
   update(objColVals, condition, cb) {
-    orm.update("cats", objColVals, condition, function(res) {
+    orm.update("products", objColVals, condition, function(res) {
       cb(res);
     });
   },
   delete(condition, cb) {
-    orm.delete("cats", condition, function(res) {
+    orm.delete("products", condition, function(res) {
       cb(res);
     });
   }
