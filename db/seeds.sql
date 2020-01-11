82b2f2695616aec9ab06f6a2f0991132e18d8935
Karan Shrestha <iamkaranshrestha96@gmail.com>
2020-01-11 10:51:15 +0100

    database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,24 +1,24 @@
 INSERT INTO products
 VALUES
-    ( 1234 , 'Tomato' , 'VEGEN_VEGETABLE' , 4);
+    ( NULL, 1234 , 'Tomato' , 'VEGEN_VEGETABLE' , 4);
 INSERT INTO customer
 VALUES
-    ( 2134 , 'Bobbi' , 'tibet' , 'lhasha' , 1909 , 'bhrikuti_marga' , 'iwillcallyousoon.com' , 9840064297 );
+    ( NULL, 2134 , 'Bobbi' , 'tibet' , 'lhasha' , 1909 , 'bhrikuti_marga' , 'iwillcallyousoon.com' , 9840064297 );
 INSERT INTO orders
 VALUES
-    ( 1 , 2134 );
+    ( NULL , 2134 );
 INSERT INTO order_details
 VALUES
-    ( 1 , 1234 );
+    (NULL , 1234 );
 INSERT INTO supermarket
 VALUES
-    ( 1 , 47 , 'EDEKA' );
+    ( NULL , 47 , 'EDEKA' );
 INSERT INTO city
 VALUES
-    ( 1 , 'Berlin' );
+    ( NULL , 'Berlin' );
 INSERT INTO category
 VALUES
-    ( 1 , 'Backery')
+    ( NULL , 'Backery');
 
 
 
